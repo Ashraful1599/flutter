da8122e746dd689a821a1c3f31bcc6bc94f904b4
Web Application Developer <smashrafulcse@gmail.com>
2022-12-23 17:52:09 +0600

    http requerst

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,131 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:dropdown_search/dropdown_search.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
-void main() => runApp(
-  MaterialApp(
-    home: MyApp(),
-  ),
-);
 
-class MyApp extends StatefulWidget {
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+//Creating a class user to store the data;
+class User {
+  final int id;
+  final int userId;
+  final String title;
+  final String body;
+
+  User({
+   required this.id,
+    required this.userId,
+    required this.title,
+    required this.body,
+  });
 }
 
 
-class _MyAppState extends State<MyApp> {
-
-  late FToast fToast;
-  Widget toast = Container(
-    padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(25.0),
-      color: Colors.greenAccent,
-    ),
-    child: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.error),
-        SizedBox(
-          width: 12.0,
-        ),
-        Text("This is a Custom Toast"),
-      ],
-    ),
-  );
-
-  _showToast() {
-    fToast.showToast(
-      child: toast,
-      gravity: ToastGravity.CENTER,
-      toastDuration: Duration(seconds: 2),
-    );
-  }
-
+class HomePage extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    fToast = FToast();
-    fToast.init(context);
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+//Applying get request.
+
+// It's for multiple posts. to work on single post follow below url to see example
+ // https://docs.flutter.dev/cookbook/networking/fetch-data
+  Future<List> getRequest() async {
+    //replace your restFull API here.
+    String url = "https://jsonplaceholder.typicode.com/posts";
+    final response = await http.get(Uri.parse(url));
+
+    var responseData = json.decode(response.body);
+
+    //Creating a list to store input data;
+    List<User> users = [];
+    for (var singleUser in responseData) {
+      User user = User(
+          id: singleUser["id"],
+          userId: singleUser["userId"],
+          title: singleUser["title"],
+          body: singleUser["body"]
+      );
+
+      //Adding user to the list.
+      users.add(user);
+    }
+    return users;
   }
 
-  String selected = "Item 1";
-  var items = [
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-    'Item 5',
-  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Custom Toasts"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-              // DropdownButton(
-              //     value: selected,
-              //     icon: Icon(Icons.arrow_drop_down),
-              //     items: items.map((String item){
-              //       return DropdownMenuItem(child: Text(item),value: item,);
-              //     }).toList(),
-              //     onChanged: (String? val){
-              //       setState(() {
-              //         selected = val!;
-              //       });
-              //     })
-            Row(
-              children: [
-                Expanded(
-                  child: DropdownSearch<int>(
-                    items: [1, 2, 3, 4, 5, 6, 7],
-                  ),
-                ),
-                Padding(padding: EdgeInsets.all(4)),
-                Expanded(
-                  child: DropdownSearch<int>.multiSelection(
-                    clearButtonProps: ClearButtonProps(isVisible: true),
-                    items: [1, 2, 3, 4, 5, 6, 7],
-                  ),
-                )
-              ],
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Http Get Request."),
+          leading: Icon(
+            Icons.get_app,
+          ),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: FutureBuilder(
+            future: getRequest(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (ctx, index) => ListTile(
+                          title: Text(snapshot.data![index].title),
+                          subtitle: Text(snapshot.data![index].body),
+                          contentPadding: EdgeInsets.only(bottom: 20.0),
+                        ),
+                      );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return Center(child: const CircularProgressIndicator());
+            },
+          ),
+          // child: FutureBuilder(
+          //   future: getRequest(),
+          //   builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+          //     if (snapshot.data == null) {
+          //       return Container(
+          //         child: Center(
+          //           child: CircularProgressIndicator(),
+          //         ),
+          //       );
+          //     } else {
+          //       return ListView.builder(
+          //         itemCount: snapshot.data.length,
+          //         itemBuilder: (ctx, index) => ListTile(
+          //           title: Text(snapshot.data[index].title),
+          //           subtitle: Text(snapshot.data[index].body),
+          //           contentPadding: EdgeInsets.only(bottom: 20.0),
+          //         ),
+          //       );
+          //     }
+          //   },
+          // ),
         ),
       ),
     );
